00757259b4cba5e27115b2196587982f414a3c40
Alexander Pfitzner <alexander.pfitzner@gmx.net>
2025-02-27 21:54:45 +0100

    Devise Setup + Navbar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,26 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    sign_out(@user)
+    redirect_to root_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :email, :image, :address)
+  end
 end
