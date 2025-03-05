7a3e57afddcbff7fafada0f9cd3ca5ef7daa97f3
Alexander Pfitzner <alexander.pfitzner@gmx.net>
2025-03-05 19:47:58 +0100

    Added Pet Validations

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -3,4 +3,9 @@ class Pet < ApplicationRecord
   belongs_to :user # owner
   has_many :users, through: :bookings # user
   has_one_attached :photo
+  validates :name, presence: true
+  validates :race, presence: true
+  validates :price_per_day, presence: true
+  validates :description, presence: true
+  validates :photo, presence: true
 end
