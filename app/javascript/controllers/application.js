ef9dc3614c2a68e21116b6b886d609f701ce8a1c
Alexander Pfitzner <alexander.pfitzner@gmx.net>
2025-03-08 10:08:09 +0100

    Fixed SCrollTo

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -7,9 +7,3 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
-
-import { Application } from '@hotwired/stimulus'
-import ScrollTo from '@stimulus-components/scroll-to'
-
-const application = Application.start()
-application.register('scroll-to', ScrollTo)
